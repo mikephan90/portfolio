2ab2dd725da314ef034dbe3b27d519899aae5bd2
Mike Phan <khoaphan90@gmail.com>
2020-09-28 18:21:54 -0400

    Updates

---
+++ precache-manifest.13d91c90b2106ea42f4b3278ab66c92f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acadb9f36a50955f7631bcc90059a160",
+    "url": "/http//mikephan.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "2c71da6b5b40e8e95672",
+    "url": "/http//mikephan.github.io/portfolio/static/css/main.4ff03400.chunk.css"
+  },
+  {
+    "revision": "b6a3fbeea97e0f82cfbd",
+    "url": "/http//mikephan.github.io/portfolio/static/js/2.028df22c.chunk.js"
+  },
+  {
+    "revision": "f4bb47abe417536e3746c07cfa9692fd",
+    "url": "/http//mikephan.github.io/portfolio/static/js/2.028df22c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c71da6b5b40e8e95672",
+    "url": "/http//mikephan.github.io/portfolio/static/js/main.444ac912.chunk.js"
+  },
+  {
+    "revision": "ef095936749b160e377c",
+    "url": "/http//mikephan.github.io/portfolio/static/js/runtime-main.3c683bb4.js"
+  },
+  {
+    "revision": "c5a2da8925e7f886eae49f84be546b8d",
+    "url": "/http//mikephan.github.io/portfolio/static/media/mikephan-resume.c5a2da89.pdf"
+  },
+  {
+    "revision": "7dd95e6761953edd2ca6926c2f36603f",
+    "url": "/http//mikephan.github.io/portfolio/static/media/natours-1.7dd95e67.jpg"
+  },
+  {
+    "revision": "8f9a21fe6a9e1d7cd733cfbdcde07f94",
+    "url": "/http//mikephan.github.io/portfolio/static/media/ohiostate.8f9a21fe.jpg"
+  }
+]);
